757ef618010c6aa96cb46b3847a7bec5813bef7a
memorand <184024952+memorand-cc@users.noreply.github.com>
2026-03-21 22:21:59 +0100

    opt

--- js/header.js
+++ js/header.js
@@ -1,20 +1,16 @@
 const imageUrls = [
-    "https://image.tmdb.org/t/p/original/73BClq9FOcrWrutnpiqhCNEWEwJ.jpg",
-    "https://image.tmdb.org/t/p/original/nummXuR5TP9pOKtWuHOSxqtOPOT.jpg",
-    "https://image.tmdb.org/t/p/original/gre7gyocZSuwd7M51CGlqsJqBmB.jpg",
-    "https://image.tmdb.org/t/p/original/qKWDHofjMHPSEOTLaixkC9ZmjTT.jpg",
-    "https://image.tmdb.org/t/p/original/fneMUcvr3qRgGA8Suv2IQ7IvDjB.jpg",
-    "https://image.tmdb.org/t/p/original/m96iaX6b4bKaD9xME9JOQghBrqC.jpg",
-	"https://image.tmdb.org/t/p/original/mdQusDes16kcHkDZQqleYCRhiNK.jpg",
-	"https://image.tmdb.org/t/p/original/yjOSpDQe8tzr8easpjTFVoQXsOX.jpg",
-    "https://image.tmdb.org/t/p/original/9qzAKt3q3dEAnmG93Bkk3Y5vPH8.jpg",
-    "https://image.tmdb.org/t/p/original/sy7Y4jCDsGPCzoQIEmxqAgPjAgr.jpg",
-    "https://image.tmdb.org/t/p/original/zm5KTTutG2oN06BehOa8uD1SVhy.jpg",
-    "https://image.tmdb.org/t/p/original/gAcqLmqzwLsmOoVw1hGODPcyTu7.jpg",
-    "https://image.tmdb.org/t/p/original/zioNFrcm4Hfw25rGjWPgEkR1iIw.jpg",
-    "https://image.tmdb.org/t/p/original/s6tSP2RKFdgnouEGKxovdpFu64o.jpg",
-    "https://image.tmdb.org/t/p/original/g3lCbTn3HYHTiFrfusxKGXMdyUW.jpg",
-    "https://image.tmdb.org/t/p/original/seueAV0oxDUk9ojz1bDYbasEu9t.jpg"   
+    "/slides/12.jpg",
+    "/slides/11.jpg",
+    "/slides/10.jpg",
+    "/slides/09.jpg",
+    "/slides/08.jpg",
+    "/slides/07.jpg",
+    "/slides/06.jpg",
+    "/slides/05.jpg",
+    "/slides/04.jpg",
+    "/slides/03.jpg",
+    "/slides/02.jpg",
+    "/slides/01.jpg"
 ];
 
 let currentIndex = 0;
